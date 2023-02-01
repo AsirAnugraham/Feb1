007e044711f047bc2f4c9011ac69c9615188982d
AsirAnugraham <97786719+AsirAnugraham@users.noreply.github.com>
2023-02-01 15:32:07 +0530

    Add files via upload

---
+++ ReplaceAllExample.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class ReplaceAllExample{  
+public static void main(String args[])
+{  
+String s1="My name is Khan. My name is Bob. My name is Sonoo.";  
+String replaceString=s1.replaceAll("is","was");  
+System.out.println(replaceString);  
+}
+} 
+
+//By using the replaceall() we can able to replace the whole thing with the given new thing
+
+
+
+
+
